3cbe98ac06eac25896ed2b6d4d12d63f4951d61a
Roman Kolesnev <rvkolesnev@gmail.com>
2014-01-13 23:40:46 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aduenko::Application.config.secret_key_base = '1b01dc630839e3ba7650f1bf011ce56aa8a2b8b4e566f4fb13f09dc264745ee74b15bb5ef100478c8472ee7d7c134466e340ab0879b031858c569c201c2f60be'
