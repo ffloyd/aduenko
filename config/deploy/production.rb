47a553519d0676190c0993aacc8f77aeb0b82c20
Roman Kolesnev <rvkolesnev@gmail.com>
2014-01-27 19:30:15 +0400

    Configure unicorn and capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,39 +1,29 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
-
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+set :user, 'aduenko'
+set :server_name, '95.85.31.11'
+server  fetch(:server_name),
+        user: fetch(:user),
+        roles: %w{web app db}
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+
+# because default is 'satging'
+set :rails_env, 'production'
+
+set :unicorn_pid, File.join(shared_path, "tmp", "pids", "unicorn.pid")
+
+# env variables for unicorn
+set :default_env, {
+  unicorn_pid:    fetch(:unicorn_pid),
+  unicorn_socket: File.join(shared_path, "tmp", "sockets", "unicorn.sock"),
+  unicorn_log:    File.join(shared_path, "log", "unicorn.log")
+}
+
+# 2 DJ workers
+set :delayed_job_args, "-n 2"
---
+++ config/unicorn/production.rb
@@ -0,0 +1,28 @@
+worker_processes 2
+timeout 15
+preload_app true
+
+pid     ENV['UNICORN_PID']
+listen  ENV['UNICORN_SOCKET']
+
+stderr_path ENV['UNICORN_LOG']
+stdout_path ENV['UNICORN_LOG']
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
